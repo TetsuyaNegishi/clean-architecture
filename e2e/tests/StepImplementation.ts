2addccc9bdb1e58495d4666cc03a772721afb8d8
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2020-08-23 16:22:39 +0900

    chore: assertライブラリをchaiに変更

--- e2e/tests/StepImplementation.ts
+++ e2e/tests/StepImplementation.ts
@@ -1,6 +1,6 @@
 
 import { Step, BeforeSuite, BeforeSpec, AfterSpec, AfterSuite } from "gauge-ts";
-import { equal } from "assert";
+import { expect } from 'chai'
 import * as puppeteer from 'puppeteer';
 
 export default class StepImplementation {
@@ -28,15 +28,6 @@ export default class StepImplementation {
         await this.page.close();
     }
 
-    @Step("Learn Reactへのリンクが表示されている")
-    public async displayLearnReact() {
-        await this.page.waitForSelector('.App-link')
-        const text = await this.page.$eval('.App-link', selector => {
-            return selector.textContent
-        })
-        equal(text, 'Learn React')
-    }
-
     @Step("<order>番目のTodoListのタイトルに<title>が表示されている")
     public async displayTodoListTitle(order: number, title: string) {
         const selector = `[data-testid=todo-title]`
@@ -45,17 +36,22 @@ export default class StepImplementation {
             return elements.map(element => element.textContent)
         })
         const actual = titleList[order - 1]
-        equal(actual, title)
+        expect(actual).to.equal(title)
     }
 
-    @Step("<order>番目のTodoListのチェックボックスが<isChecked>である")
-    public async displayTodoListCheckbox(order: number, isChecked: string) {
+    @Step("<order>番目のTodoListのチェックボックスが<checked>である")
+    public async displayTodoListCheckbox(order: number, checked: string) {
+        const isChecked = this.transformStringToBoolean(checked)
         const selector = `[data-testid=todo-checkbox] input[type=radio]`
         await this.page.waitFor(selector)
         const checkedList = await this.page.$$eval(selector, elements => {
             return elements.map(element => element.checked)
         })
         const actual = checkedList[order - 1]
-        equal(actual, isChecked.toLowerCase() === "true")
+        expect(actual).to.equal(isChecked)
+    }
+
+    private transformStringToBoolean(text: string): boolean {
+        return text.toLowerCase() === "true"
     }
 }
